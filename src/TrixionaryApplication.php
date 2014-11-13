c157abf04ce2ac7d6ad64dddf1e6165f225bac40
gossi <github@gos.si>
2014-11-13 22:12:31 +0100

    init

---
+++ src/TrixionaryApplication.php
@@ -0,0 +1,21 @@
+<?php
+namespace gossi\trixionary\app;
+
+use keeko\core\application\AbstractApplication;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Trixionary App
+ * 
+ * @license MIT
+ * @author gossi
+ */
+class TrixionaryApplication extends AbstractApplication {
+
+	/**
+	 * @param Request $request
+	 * @param string $path
+	 */
+	public function run(Request $request, $path) {
+	}
+}
